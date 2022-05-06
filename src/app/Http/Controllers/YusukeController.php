4f95350e62f2e161468cf3e4e124ae1a6950b727
araiyusuke <info.yusuke.arai@gmail.com>
2022-05-06 09:55:46 +0900

    テストページを作成

--- src/app/Http/Controllers/YusukeController.php
+++ src/app/Http/Controllers/YusukeController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class YusukeController extends Controller
 {
-    //
+    public function index () 
+    {
+        $hello = 'Hello,World!';
+        return view('yusuke.index', compact('hello'));
+    }
 }
---
+++ src/resources/views/yusuke/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+ 
+<head>
+    <meta charset="utf-8" />
+    <title>userページ</title>
+</head>
+ 
+<body>
+    <h1>テスト</h1>
+    <p>{{ $hello }}</p>
+</body>
+ 
+</html>
